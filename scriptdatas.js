b4ef771cafeed402060ee37ddbed5307feb506de
Camila Azevedo <92113919+Camilay3@users.noreply.github.com>
2022-01-11 21:15:29 -0300

    Segundo commit
    Atualização de ampliar imagem

--- scriptdatas.js
+++ scriptdatas.js
@@ -1,3 +1,21 @@
+let imagens = document.querySelectorAll('.imgi');
+let modal = document.querySelector('.modal');
+let modalImg = document.querySelector('#modal_img');
+let btClose = document.querySelector('#bt_close');
+let srcVal = "";
+
+for (let a=0; a < imagens.length; a++) {
+    imagens[a].addEventListener('click', function abrir(){
+        srcVal = imagens[a].getAttribute('src');
+        modalImg.setAttribute('src', srcVal);
+        modal.classList.toggle('modal_active');
+    })
+}
+
+btClose.addEventListener('click', function fechar(){
+    modal.classList.toggle('modal_active');
+})
+
 /* var label1 = document.getElementById('l1');
 var label2 = document.getElementById('l2');
 var label3 = document.getElementById('l3');
